e05579f408d3333b6176c7c2b07bbf144b7912ca
Michael Arnaldi <arnaldimichael@gmail.com>
2022-01-16 16:59:35 +0000

    chore: patch commit, summary of work.
    Co-authored-by: Peter Krol <peter@peterkrol.com>

---
+++ testing/packages/demo/src/fluent-guard.ts
@@ -0,0 +1,14 @@
+import { Either } from "@tsplus/stdlib/data/Either"
+
+declare const either: Either<string, number>
+
+if (either.isLeft()) {
+    const x: string = either.left
+    console.log(x)
+}
+
+
+if (either.isRight()) {
+    const x: number = either.right
+    console.log(x)
+}
