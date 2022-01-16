2f572be9f7b51aaafef615d87c360af42bff4749
Michael Arnaldi <arnaldimichael@gmail.com>
2022-01-16 16:59:35 +0000

    chore: patch commit, summary of work.

---
+++ effect/packages/package1/src/companions-everywhere.ts
@@ -0,0 +1,15 @@
+/**
+ * @tsplus type companions-everwhere/A
+ * @tsplus companion companions-everywhere/AOps
+ */
+export type A = {}
+
+/**
+ * @tsplus static companions-everywhere/AOps get
+ */
+export const get: A = {}
+
+/**
+ * @tsplus static companions-everywhere/AOps __call
+ */
+export const callA = (): A => ({})
--- src/services/documentHighlights.ts
+++ src/services/documentHighlights.ts
@@ -79,6 +79,8 @@ import {
     toArray,
     toPath,
     TryStatement,
+    BinaryExpression,
+    isBinaryExpression,
 } from "./_namespaces/ts";
 
 export interface DocumentHighlights {
@@ -96,6 +98,17 @@ export namespace DocumentHighlights {
             const { openingElement, closingElement } = node.parent.parent;
             const highlightSpans = [openingElement, closingElement].map(({ tagName }) => getHighlightSpanForNode(tagName, sourceFile));
             return [{ fileName: sourceFile.fileName, highlightSpans }];
+        }
+
+        program.getTypeChecker().findAndCheckDoAncestor(node)
+        let currentBinaryAnchestor: BinaryExpression | undefined = findAncestor(node, isBinaryExpression);
+        let binaryExpressionParent = currentBinaryAnchestor;
+        while (currentBinaryAnchestor) {
+            binaryExpressionParent = currentBinaryAnchestor;
+            currentBinaryAnchestor = findAncestor(currentBinaryAnchestor.parent, isBinaryExpression);
+        }
+        if (binaryExpressionParent) {
+            program.getTypeChecker().getTypeAtLocation(binaryExpressionParent);
         }
 
         return getSemanticDocumentHighlights(position, node, program, cancellationToken, sourceFilesToSearch) || getSyntacticDocumentHighlights(node, sourceFile);
