2edf5bc2a23544ef7c0d21ebf89adae0cd9bf831
Michael Arnaldi <arnaldimichael@gmail.com>
2022-01-16 16:59:35 +0000

    chore: patch commit, summary of work.
    Co-authored-by: Peter Krol <peter@peterkrol.com>

--- src/services/signatureHelp.ts
+++ src/services/signatureHelp.ts
@@ -77,8 +77,41 @@ namespace ts.SignatureHelp {
                 if (onlyUseSyntacticOwners && !isSyntacticOwner(startingToken, invocation.node, sourceFile)) {
                     return undefined;
                 }
-                const candidates: Signature[] = [];
-                const resolvedSignature = checker.getResolvedSignatureForSignatureHelp(invocation.node, candidates, argumentCount)!; // TODO: GH#18217
+                let candidates: Signature[] = [];
+                let resolvedSignature = checker.getResolvedSignatureForSignatureHelp(invocation.node, candidates, argumentCount)!; // TODO: GH#18217
+                // TSPLUS EXTENSION BEGIN
+                if(resolvedSignature.declaration && isFunctionDeclaration(resolvedSignature.declaration) && resolvedSignature.parameters.every(isSymbolParameterDeclaration)) {
+                    const declaration = resolvedSignature.declaration;
+                    const lastParam = declaration.parameters[declaration.parameters.length - 1];
+                    const parameterCount = resolvedSignature.thisParameter ? declaration.parameters.length - 1 : declaration.parameters.length;
+                    if(declaration.name && lastParam && isIdentifier(lastParam.name) && lastParam.name.escapedText.toString() === "___tsplusTrace" && argumentCount < parameterCount) {
+                        const untracedDeclaration = factory.createFunctionDeclaration(
+                            declaration.decorators,
+                            declaration.modifiers,
+                            declaration.asteriskToken,
+                            declaration.name,
+                            declaration.typeParameters,
+                            declaration.parameters.slice(0, declaration.parameters.length - 1),
+                            declaration.type,
+                            undefined
+                        );
+                        setParent(untracedDeclaration, declaration.parent);
+                        untracedDeclaration.jsDoc = declaration.jsDoc;
+                        const untracedSignature = checker.createSignature(
+                            untracedDeclaration,
+                            resolvedSignature.typeParameters,
+                            resolvedSignature.thisParameter,
+                            resolvedSignature.parameters.slice(0, resolvedSignature.parameters.length - 1),
+                            resolvedSignature.getReturnType(),
+                            resolvedSignature.resolvedTypePredicate,
+                            resolvedSignature.minArgumentCount,
+                            resolvedSignature.flags
+                        );
+                        resolvedSignature = untracedSignature;
+                        candidates = [untracedSignature];
+                    }
+                }
+                // TSPLUS EXTENSION END
                 return candidates.length === 0 ? undefined : { kind: CandidateOrTypeKind.Candidate, candidates, resolvedSignature };
             }
             case InvocationKind.TypeArgs: {
