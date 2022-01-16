14d424b58a8b69ddd05afb383154f4f068735ec8
Michael Arnaldi <arnaldimichael@gmail.com>
2022-01-16 16:59:35 +0000

    chore: patch commit, summary of work.

--- src/services/completions.ts
+++ src/services/completions.ts
@@ -2185,6 +2185,17 @@ namespace ts.Completions {
                 }
             }
 
+            // TSPLUS EXTENSION START
+            if (isExpression(node)) {
+                const extensions = typeChecker.getExtensions(node);
+                if (extensions) {
+                    extensions.forEach((extension) => {
+                        addPropertySymbol(extension, /* insertAwait */ false, /* insertQuestionDot */ false);
+                    });
+                }
+            }
+            // TSPLUS EXTENSION END
+
             if (insertAwait && preferences.includeCompletionsWithInsertText) {
                 const promiseType = typeChecker.getPromisedTypeOfPromise(type);
                 if (promiseType) {
