12d54dbc440c847b150ca0f565efd1a037336342
Michael Arnaldi <arnaldimichael@gmail.com>
2022-01-16 16:59:35 +0000

    chore: patch commit, summary of work.
    Co-authored-by: Peter Krol <peter@peterkrol.com>

---
+++ effect/packages/package1/src/union-inheritance.ts
@@ -0,0 +1,136 @@
+/**
+ * @tsplus type union/Cons
+ */
+export class Cons<A> implements Iterable<A> {
+  readonly _tag = "Cons";
+  constructor(readonly head: A, readonly tail: List<A>) {}
+  // @ts-expect-error
+  [Symbol.iterator]() {}
+}
+
+/**
+ * @tsplus type union/Nil
+ */
+export class Nil<A> implements Iterable<A> {
+  readonly _tag = "Nil";
+  // @ts-expect-error
+  [Symbol.iterator]() {}
+}
+
+/**
+ * @tsplus type union/List
+ */
+export type List<A> = Cons<A> | Nil<A>;
+
+/**
+ * @tsplus fluent union/List map
+ */
+export declare function mapList<A, B>(list: List<A>, f: (a: A) => B): List<B>;
+
+declare const x: List<string>;
+
+function transformTest() {
+  x.map(s => s);
+}
+
+/**
+ * @tsplus type union/A
+ */
+export interface A {
+  readonly _A: unique symbol;
+}
+
+/**
+ * @tsplus type union/B
+ */
+export interface B {
+  readonly _B: unique symbol;
+}
+
+/**
+ * @tsplus type union/C
+ */
+export interface C extends A {
+  readonly _C: unique symbol;
+}
+
+/**
+ * @tsplus type union/D
+ */
+export interface D extends A, B {
+  readonly _D: unique symbol;
+}
+
+/**
+ * @tsplus type union/E
+ */
+export type E = C | D;
+
+/**
+ * @tsplus fluent union/A methodA
+ */
+export declare function methodA(self: A): void;
+
+/**
+ * @tsplus fluent union/B methodB
+ */
+export declare function methodB(self: B): void;
+
+/**
+ * @tsplus fluent union/E methodE
+ */
+export declare function methodE(self: E): void;
+
+declare const e: E;
+
+function subtypeTest() {
+  e.methodE();
+  e.methodA();
+  // @ts-expect-error
+  e.methodB();
+}
+
+// alias inheritance
+
+/**
+ * @tsplus type union/AA
+ */
+export interface AA {
+  readonly _AA: unique symbol;
+}
+
+/**
+ * @tsplus type union/AB
+ */
+export interface AB extends AA {
+  readonly _AB: unique symbol;
+}
+
+/**
+ * @tsplus type union/AC
+ */
+export type AC = AA
+
+export type AD = AC | AB
+
+/**
+ * @tsplus fluent union/AA methodAA
+ */
+export declare function methodAA(self: AA): void
+/**
+ * @tsplus fluent union/AB methodAB
+ */
+export declare function methodAB(self: AB): void
+/**
+ * @tsplus fluent union/AC methodAC
+ */
+export declare function methodAC(self: AC): void
+
+declare const ac: AD
+
+function aliasTest() {
+  ac.methodAA()
+  // @ts-expect-error
+  ac.methodAB()
+  ac.methodAC()
+}
