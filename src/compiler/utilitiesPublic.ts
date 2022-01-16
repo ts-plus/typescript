cb4c29f5ae8f16d3c5b811901119c9368b6926f4
Michael Arnaldi <arnaldimichael@gmail.com>
2022-01-16 16:59:35 +0000

    chore: patch commit, summary of work.
    Co-authored-by: Peter Krol <peter@peterkrol.com>

--- src/compiler/utilitiesPublic.ts
+++ src/compiler/utilitiesPublic.ts
@@ -2058,4 +2058,10 @@ namespace ts {
     }
 
     // #endregion
+
+    // TSPLUS EXTENSION START
+    export function isTsPlusUniqueIdentifier(node: Identifier): node is TsPlusUniqueIdentifier {
+        return !!(node as TsPlusUniqueIdentifier).tsPlusUniqueIdentifier;
+    }
+    // TSPLUS EXTENSION END
 }
