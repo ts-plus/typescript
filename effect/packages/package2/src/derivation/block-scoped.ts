080642e4412f347838bf61203610ee40fd301c01
Michael Arnaldi <arnaldimichael@gmail.com>
2022-01-16 16:59:35 +0000

    chore: patch commit, summary of work.
    Co-authored-by: Peter Krol <peter@peterkrol.com>

---
+++ effect/packages/package2/src/derivation/block-scoped.ts
@@ -0,0 +1,49 @@
+import { Show } from "./show";
+
+export function f<A>(/** @tsplus implicit local */_show: Show<A>): Show<Maybe<A>> {
+    const ok: Show<Maybe<A>> = Derive()
+    return ok;
+}
+
+export function g<A>(/** @tsplus implicit local */_show: Show<A>): Show<Maybe<A>> {
+    const go = () => {
+        const _show = 0;
+        _show;
+        const ok: Show<Maybe<A>> = Derive()
+        return ok;
+    }
+    return go();
+}
+
+/**
+ * @tsplus implicit local
+ */
+const _showSourceScoped: Show<{ x: number }> = Derive()
+
+export function h() {
+  // uses _showSourceScoped
+  const ok: Show<{ x: number }> = Derive()
+  return ok;
+}
+
+export function i() {
+  /**
+   * @tsplus implicit local
+   */
+  const _showBlockScoped: Show<{ x: number }> = Derive();
+  const ok: Show<{ x: number }> = Derive();
+  return ok;
+}
+
+const id = <A>(a: () => A) => a()
+
+export function j() {
+  return id(() => {
+    /** @tsplus implicit local */
+    const _showBlockScoped: Show<{ x: number }> = Derive();
+    const ok: Show<{ x: number }> = Derive()
+    if (Math.random() > 0 && ok) {
+      return 0
+    }
+  })
+}
