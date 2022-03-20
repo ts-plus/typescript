cefba238e0512234701a7e9b5988b6cf4515ec93
Michael Arnaldi <arnaldimichael@gmail.com>
2022-03-20 14:51:02 +0000

    chore: test resolution

--- effect/packages/package1/src/prelude/definition/List.ts
+++ effect/packages/package1/src/prelude/definition/List.ts
@@ -3,7 +3,7 @@
  * @tsplus companion tsplus-tests/ListOps
  */
 export class List<A> {
-    constructor(readonly array: readonly A[]) {}
+    constructor(readonly array: readonly A[]) { }
 }
 
 /**
@@ -31,15 +31,24 @@ export function make<As extends readonly any[]>(...as: As): List<As[number]> {
 
 /**
  * @tsplus operator tsplus-tests/List +
+ * @tsplus fluent tsplus-tests/List concat
  */
-export function prependTo<A>(a: A, self: List<A>): List<A> {
+export function concat<A, B>(self: List<A>, that: List<B>): List<A | B> {
+    return new List([...self.array, ...that.array])
+}
+
+/**
+ * Prepends `a: B` to `List<A>`
+ * @tsplus operator tsplus-tests/List +
+ */
+export function prependTo<A, B>(a: A, self: List<B>): List<A | B> {
     return new List([a, ...self.array])
 }
 
 /**
  * @tsplus fluent tsplus-tests/List prepend
  */
-export function prepend<A>(self: List<A>, a: A): List<A> {
+export function prepend<A, B>(self: List<A>, a: B): List<A | B> {
     return new List([a, ...self.array])
 }
 
@@ -47,9 +56,10 @@ export function prepend<A>(self: List<A>, a: A): List<A> {
  * @tsplus operator tsplus-tests/List +
  * @tsplus fluent tsplus-tests/List append
  */
-export function append<A>(self: List<A>, a: A): List<A> {
+export function append<A, B>(self: List<A>, a: B): List<A | B> {
     return new List([...self.array, a])
 }
 
-export const prepended = (1 + List(0)).append(0).prepend(1)
-export const appended = (List(0) + 1).append(0).prepend(1)
+export const prepended = 1 + List(0)
+export const appended = List(0) + 1
+export const sequenced = List(0) + List(1)
