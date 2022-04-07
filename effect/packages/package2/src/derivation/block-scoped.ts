765926b4062ac06172fda52fa318ef91730ae9f2
Michael Arnaldi <arnaldimichael@gmail.com>
2022-04-07 19:36:44 +0000

    repro bad unique

--- effect/packages/package2/src/derivation/block-scoped.ts
+++ effect/packages/package2/src/derivation/block-scoped.ts
@@ -1,18 +1,18 @@
 import { Show } from "./show";
 
 export function f<A>(/** @tsplus implicit local */_show: Show<A>): Show<Maybe<A>> {
-    const ok: Show<Maybe<A>> = Derive()
-    return ok;
+  const ok: Show<Maybe<A>> = Derive()
+  return ok;
 }
 
 export function g<A>(/** @tsplus implicit local */_show: Show<A>): Show<Maybe<A>> {
-    const go = () => {
-        const _show = 0;
-        _show;
-        const ok: Show<Maybe<A>> = Derive()
-        return ok;
-    }
-    return go();
+  const go = () => {
+    const _show = 0;
+    _show;
+    const ok: Show<Maybe<A>> = Derive()
+    return ok;
+  }
+  return go();
 }
 
 /**
@@ -33,4 +33,17 @@ export function i() {
   const _showBlockScoped: Show<{ x: number }> = Derive();
   const ok: Show<{ x: number }> = Derive();
   return ok;
+}
+
+const id = <A>(a: () => A) => a()
+
+export function j() {
+  return id(() => {
+    /** @tsplus implicit local */
+    const _showBlockScoped: Show<{ x: number }> = Derive();
+    const ok: Show<{ x: number }> = Derive()
+    if (Math.random() > 0 && ok) {
+      return 0
+    }
+  })
 }
